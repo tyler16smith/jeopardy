371f580790c0124ef06a0c019b94c2d5b0edcf3a
Tyler Smith <tylersmithlf@gmail.com>
2024-04-22 19:20:03 -0600

    Fixes typing on document header

--- src/components/DocumentHead.tsx
+++ src/components/DocumentHead.tsx
@@ -18,30 +18,30 @@ const DocumentHeader: React.FC<DocumentHeaderProps> = ({
   const url = typeof window !== 'undefined' ? window.location.href : ''
   return (
     <Head>
-      <title>{title || 'Play Jeopardy'}</title>
+      <title>{title ?? 'Play Jeopardy'}</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
       />
-      <link rel="icon" href={iconPath || '/j.png'} />
+      <link rel="icon" href={iconPath ?? '/j.png'} />
 
       {/* Open Graph tags */}
-      <meta property="og:title" content={title || 'Play Jeopardy'} />
-      <meta property="og:type" content={ogType || 'website'} />
+      <meta property="og:title" content={title ?? 'Play Jeopardy'} />
+      <meta property="og:type" content={ogType ?? 'website'} />
       <meta property="og:url" content={url} />
       <meta
         property="og:image"
-        content={ogImage || iconPath || '/j.png'}
+        content={ogImage ?? iconPath ?? '/j.png'}
       />
-      <meta property="og:description" content={description || 'Play Jeopardy'} />
+      <meta property="og:description" content={description ?? 'Play Jeopardy'} />
 
       {/* Twitter Meta Tags */}
       <meta name="twitter:card" content="Play Jeopardy" />
-      <meta name="twitter:title" content={title || 'Play Jeopardy'} />
-      <meta name="twitter:description" content={description || 'Play Jeopardy'} />
+      <meta name="twitter:title" content={title ?? 'Play Jeopardy'} />
+      <meta name="twitter:description" content={description ?? 'Play Jeopardy'} />
       <meta
         name="twitter:image"
-        content={ogImage || iconPath || '/j.png'}
+        content={ogImage ?? iconPath ?? '/j.png'}
       />
     </Head>
   )
